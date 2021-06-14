16cbb47f2d6e1573f9071981c44b038cb9a75a56
Martina <martinaescorial9@gmail.com>
2021-06-14 16:36:57 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 5;
+            Console.WriteLine("N = {0}", N);
+            double A = 10.17;
+            Console.WriteLine("A = {0}", A);
+            char C = 'b';
+            Console.WriteLine("C = {0}", C);
+            Console.WriteLine("Suma = {0}", N + A);
+            Console.WriteLine("Resta = {0}", A - N);
+            Console.WriteLine(C + ": " + (int)C);
+        }
+    }
+}
